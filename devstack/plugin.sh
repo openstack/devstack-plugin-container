63c7b8eddc22a57975fe0f3682c891bfb53956a0
Michał Dulko <mdulko@redhat.com>
2018-11-27 12:51:48 +0100

    Add support for CRI-O as container engine
    This commit adds support for CRI-O. Support for Fedora/CentOS is in
    progress.
    
    Change-Id: Ib049d66058429e499f5d0932c4a749820bec73ff

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -6,6 +6,7 @@ set -o xtrace
 
 echo_summary "container's plugin.sh was called..."
 source $DEST/devstack-plugin-container/devstack/lib/docker
+source $DEST/devstack-plugin-container/devstack/lib/crio
 (set -o posix; set)
 
 if is_service_enabled container; then
@@ -13,17 +14,23 @@ if is_service_enabled container; then
         echo_summary "Installing container engine"
         if [[ ${CONTAINER_ENGINE} == "docker" ]]; then
             check_docker || install_docker
+        elif [[ ${CONTAINER_ENGINE} == "crio" ]]; then
+            check_crio || install_crio
         fi
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         echo_summary "Configuring container engine"
         if [[ ${CONTAINER_ENGINE} == "docker" ]]; then
             configure_docker
+        elif [[ ${CONTAINER_ENGINE} == "crio" ]]; then
+            configure_crio
         fi
     fi
 
     if [[ "$1" == "unstack" ]]; then
         if [[ ${CONTAINER_ENGINE} == "docker" ]]; then
             stop_docker
+        elif [[ ${CONTAINER_ENGINE} == "crio" ]]; then
+            stop_crio
         fi
     fi
 
